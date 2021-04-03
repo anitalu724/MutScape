47e936e58468c55efc60d5ee40557ea17da03e00
anitalu724 <b06901011@ntu.edu.tw>
2021-04-03 18:24:47 +0800

    :tada: 20210403_all_updates

--- mutscape/installation/install_module.sh
+++ mutscape/installation/install_module.sh
@@ -5,6 +5,9 @@ pip3 install numpy
 pip3 install pandas
 pip3 install vcfpy
 pip3 install pysam
+conda config --add channels r
+conda config --add channels bioconda
+conda install pysam
 pip3 install seaborn
 pip3 install oncodriveclust
 pip3 install sklearn
