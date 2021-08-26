92da2521f652f924d612836402bab81ceda254d5
anitalu724 <b06901011@ntu.edu.tw>
2021-08-26 19:38:18 +0800

    20210826_testMS

--- mutscape/lib/analysis/mutational_sig.py
+++ mutscape/lib/analysis/mutational_sig.py
@@ -405,11 +405,11 @@ class MutationalSignature:
                 P[t-1], Z[t-1] = t, 0
 
                 PP = np.where(P != 0)[0]+1
-                Pn = [colName[x-1] for x in PP]
+                # Pn = [colName[x-1] for x in PP]
                 ZZ = np.where(Z != 0)[0]+1
 
                 CP = np.zeros(C.shape)
-                CP[:, PP-1] = C[:, Pn]
+                CP[:, PP-1] = C.iloc[:, PP-1]
                 
                 CP[:, ZZ-1] = np.zeros((m, msize(ZZ, 1)))
 
@@ -434,7 +434,7 @@ class MutationalSignature:
                     Pn = [colName[x-1] for x in PP]
                     ZZ = np.where(Z != 0)[0]+1
 
-                    CP[:, PP-1] = C[:, Pn]
+                    CP[:, PP-1] = C.iloc[:, PP-1]
                     CP[:, ZZ-1] = np.zeros((m, msize(ZZ, 1)))
 
                     z = np.dot(np.linalg.pinv(CP), d)
