6a6c139d9c26b0f0b820d57987bfa28c6b49abeb
anitalu724 <b06901011@ntu.edu.tw>
2021-08-26 23:17:07 +0800

    20210826_testMS

--- mutscape/lib/analysis/mutational_sig.py
+++ mutscape/lib/analysis/mutational_sig.py
@@ -426,7 +426,7 @@ class MutationalSignature:
         lsq_reconstructed = pd.DataFrame(index=range(n_feature),columns=range(n_samples))
         print(lsq_contribution.shape)
         print(lsq_contribution)
-        print(lsq_contribution[:, 0])
+        print(lsq_contribution.iloc[:, 0])
         os._exit(0)
 
         for i in range(n_samples):
