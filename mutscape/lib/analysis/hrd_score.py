af7660585df6c5b380902a1b829ab0c1a7a60453
anitalu724 <b06901011@ntu.edu.tw>
2021-09-06 17:51:44 +0800

    20210906_ms

--- mutscape/lib/analysis/hrd_score.py
+++ mutscape/lib/analysis/hrd_score.py
@@ -121,7 +121,7 @@ class HRDCompare:
             # check if chrx,y exists or total=2&A_cn=1&B_cn=1
             tmp_df = pd.read_csv(i, sep = '\t')
             print(tmp_df)
-            chrx = tmp_df.loc[tmp_df['CHromosome'] == 'chrX' or tmp_df['CHromosome'] == 'chrY']
+            chrx = tmp_df.loc[tmp_df['Chromosome'] == 'chrX' or tmp_df['CHromosome'] == 'chrY']
             if chrx.shape[0] != 0:
                 print(chrx)
                 os._exit(0)
