010dfe8070a75c8edb484a7d4ce5739749fe8d47
anitalu724 <b06901011@ntu.edu.tw>
2021-09-06 18:04:54 +0800

    20210906_ms

--- mutscape/lib/analysis/hrd_score.py
+++ mutscape/lib/analysis/hrd_score.py
@@ -127,7 +127,7 @@ class HRDCompare:
             delete = pd.concat([chrx, chry, total2]).drop_duplicates().reset_index(drop=True)
             if delete.shape[0] != 0:
                 print(tmp_df.shape, delete.shape)
-                tmp_df = tmp_df.drop(delete.index)
+                tmp_df = tmp_df.drop(delete)
                 print(tmp_df)
                 os._exit(0)
             
