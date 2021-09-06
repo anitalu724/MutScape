a6e3a69644d0603ca28826706ab9c2178c681c22
anitalu724 <b06901011@ntu.edu.tw>
2021-09-06 18:03:36 +0800

    20210906_ms

--- mutscape/lib/analysis/hrd_score.py
+++ mutscape/lib/analysis/hrd_score.py
@@ -126,8 +126,9 @@ class HRDCompare:
             total2 = tmp_df.loc[tmp_df['total_cn'] == 2].loc[tmp_df['A_cn'] == 1].loc[tmp_df['B_cn'] == 1]
             delete = pd.concat([chrx, chry, total2]).drop_duplicates().reset_index(drop=True)
             if delete.shape[0] != 0:
-                print(delete)
-                os._exit(0)
+                print(tmp_df.shape, delete.shape)
+                tmp_df = tmp_df.drop(delete.index)
+                print(tmp_df)
             
             
             scar_r.write("scar_score(\"" + i + "\", reference = \""+ref+"\", seqz = FALSE, outputdir = \"" + folder[:-1] + "\")\n")
