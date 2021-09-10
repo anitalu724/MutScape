7d9b5f6250177bdb60ffad50ff6195559e59fb1e
anitalu724 <b06901011@ntu.edu.tw>
2021-09-10 14:23:19 +0800

    20210910_hrd

--- mutscape/lib/analysis/hrd_score.py
+++ mutscape/lib/analysis/hrd_score.py
@@ -234,13 +234,13 @@ class HRDCompare:
         sns.set_style('white')
         grid_kws = {'height_ratios': (.9, .2),'hspace': 0.3}  
         f, (ax, cbar_ax) = plt.subplots(2,figsize=(20,6), gridspec_kw=grid_kws)
-        # , xticklabels =aux_list, yticklabels = my_list
-        ax = sns.heatmap(M, vmin=-0.5, vmax = 1.5, square = True,yticklabels = self.type, linewidth = 1, cbar_ax = cbar_ax, ax = ax,
+        # , xticklabels =aux_list, yticklabels = my_list, cbar_ax = cbar_ax, 
+        ax = sns.heatmap(M, vmin=-0.5, vmax = 1.5, square = True,yticklabels = self.type, linewidth = 1, ax = ax,
                             cmap='Blues',cbar_kws={'orientation': 'horizontal','shrink':1, 'aspect':70})
         # ax.set_title('Cosine Similarity',fontsize=TITLE_SIZE,weight='bold',pad=0,verticalalignment='bottom')
-        ax.set_xticklabels(ax.get_xticklabels(),rotation=90, horizontalalignment='center', fontsize=20, color='#222222')
+        # ax.set_xticklabels(ax.get_xticklabels(),rotation=90, horizontalalignment='center', fontsize=20, color='#222222')
         ax.tick_params(axis='both',length=0)
-        ax.set_yticklabels(ax.get_yticklabels(), fontsize=20,color='#222222',verticalalignment='center')
+        ax.set_yticklabels(ax.get_yticklabels(), rotation=90,fontsize=20,color='#222222',verticalalignment='center')
         plt.ylim(bottom=0, top=len(wgdList)+0.5)
         plt.savefig(pic+'WGD_heatmap.pdf',dpi=300,bbox_inches='tight')
         plt.clf()
