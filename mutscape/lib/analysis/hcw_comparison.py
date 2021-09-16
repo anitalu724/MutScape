74ee63dbe59ecac0ea10133a877775e30ce6c04e
anitalu724 <b06901011@ntu.edu.tw>
2021-09-17 01:11:09 +0800

    20210916_hcwc

--- mutscape/lib/analysis/hcw_comparison.py
+++ mutscape/lib/analysis/hcw_comparison.py
@@ -243,9 +243,9 @@ class HCWComparison:
         # colorbar.set_ticklabels(['Non-WGD' , 'WGD'])                       
         # colorbar.ax.tick_params(labelsize = LABEL_SIZE + 12)    
 
-        tmp_plot1,  = ax.plot([], [], c = '#b7d5ea' , marker='s', markersize=10, fillstyle='left', linestyle='none', mec = 'None')
-        tmp_plot2,  = ax.plot([], [], c = '#266199' , marker='s', markersize=10, fillstyle='left', linestyle='none', mec = 'None')
-        ax.legend((tmp_plot1, tmp_plot2), ('Non-WGD','WGD'), labelspacing=0.5, loc='right', fontsize=LABEL_SIZE-2, edgecolor='white')
+        tmp_plot1,  = ax.plot([], [], c = '#b7d5ea' , marker='s', markersize=10, fillstyle='full', linestyle='none', mec = 'None')
+        tmp_plot2,  = ax.plot([], [], c = '#266199' , marker='s', markersize=10, fillstyle='full', linestyle='none', mec = 'None')
+        ax.legend((tmp_plot1, tmp_plot2), ('Non-WGD','WGD'), labelspacing=0.5, loc='best', fontsize=LABEL_SIZE-2, edgecolor='white')
 
         ax.tick_params(axis='both',length=0)
         ax.set_yticklabels(ax.get_yticklabels(), color='#222222', rotation = 'horizontal', fontsize=LABEL_SIZE, fontweight = 'bold')
