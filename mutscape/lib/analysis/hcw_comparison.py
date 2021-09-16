0ab0c4c6342c058c75b32a10c12983445d588e99
anitalu724 <b06901011@ntu.edu.tw>
2021-09-17 01:18:16 +0800

    20210916_hcwc

--- mutscape/lib/analysis/hcw_comparison.py
+++ mutscape/lib/analysis/hcw_comparison.py
@@ -245,10 +245,11 @@ class HCWComparison:
 
         tmp_plot1,  = ax.plot([], [], c = '#b7d5ea' , marker='s', markersize=10, fillstyle='full', linestyle='none', mec = 'None')
         tmp_plot2,  = ax.plot([], [], c = '#266199' , marker='s', markersize=10, fillstyle='full', linestyle='none', mec = 'None')
-        ax.legend((tmp_plot1, tmp_plot2), ('Non-WGD','WGD'), labelspacing=0.5, loc='best', fontsize=LABEL_SIZE-2, edgecolor='white')
+        ax.legend((tmp_plot1, tmp_plot2), ('Non-WGD','WGD'), labelspacing=0.5, loc='best', fontsize=LABEL_SIZE-4, edgecolor='white', bbox_to_anchor=(1.2, 0.5))
 
         ax.tick_params(axis='both',length=0)
         ax.set_yticklabels(ax.get_yticklabels(), color='#222222', rotation = 'horizontal', fontsize=LABEL_SIZE, fontweight = 'bold')
+        ax.set_xticklabels(ax.get_xticklabels(), color='#222222', rotation = 'horizontal', fontsize=LABEL_SIZE-8)
         
         # plt.ylim(bottom=0, top=len(wgdList))
         plt.savefig(pic+'WGD_heatmap.pdf',dpi = 300, bbox_inches='tight')
