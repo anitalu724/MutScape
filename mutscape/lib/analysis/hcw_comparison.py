e0fd83a6e1c233b8cb3d2e9f0a864897e115965e
anitalu724 <b06901011@ntu.edu.tw>
2021-09-17 14:35:23 +0800

    20210917_hcwc

--- mutscape/lib/analysis/hcw_comparison.py
+++ mutscape/lib/analysis/hcw_comparison.py
@@ -122,7 +122,7 @@ class HCWComparison:
     def __init__(self, file):
         print(colored(("\nStart analysing HRD_CIN_WGD Comparison...."), 'yellow'))
         df = (pd.read_csv(file, sep='\t', index_col=None)).dropna(axis='columns')
-        df = df.sort_values(by=['PathR', 'SampleID'], ascending=True).sort_values(by=['PathR'], ascending=False)
+        df = df.sort_values(by=['PathR', 'SampleID'], ascending=[False, True]])
         # 
         print(df)
         os._exit(0)
