8c1bd545fcb65a185549331bdd9ec628d57404dd
anitalu724 <b06901011@ntu.edu.tw>
2021-09-20 20:58:15 +0800

    20210920_quickInstall

--- mutscape/installation/quickInstall_1.sh
+++ mutscape/installation/quickInstall_1.sh
@@ -5,15 +5,18 @@ echo -e "\e[1;33m Press enter to continue... \e[0m"
 read -n 1 input
 if [ "$input" = "" ]
 then
-    echo -e "\e[1;35m Install Ensembl's VEP \n \e[0m"
+    echo -e "\e[1;35m \nInstall Ensembl's VEP \n \e[0m"
     conda install -c bioconda -c conda-forge samtools=1.10 ucsc-liftover=377 bcftools=1.10.2
     conda install -c bioconda -c conda-forge -c defaults ensembl-vep=102.0 
 
-    echo -e "Install vcf2maf and move MutScape tool into vcf2maf"
+    echo -e "\e[1;35m \nInstall vcf2maf \n \e[0m"
     wget https://github.com/mskcc/vcf2maf/archive/refs/tags/v1.6.20.tar.gz
     tar -zxf v1.6.20.tar.gz
+
+    echo -e "\e[1;35m \nMove MutScape tool into vcf2maf\n \e[0m"
     mv MutScape vcf2maf-1.6.20/
     cd vcf2maf-1.6.20
+    perl vcf2maf.pl --man
 
 else
     echo "Quit"
