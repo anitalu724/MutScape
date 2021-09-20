6600230e2e6fb3d07f51097b1a5f082a54afd0b2
anitalu724 <b06901011@ntu.edu.tw>
2021-09-20 20:31:13 +0800

    20210920_quickInstall

--- mutscape/installation/quickInstall_1.sh
+++ mutscape/installation/quickInstall_1.sh
@@ -2,7 +2,7 @@ echo -e "\e[1;33m \nThis is quickInstall_1.sh\n \e[0m"
 
 echo -e "\e[1;33m \nWARNING: Before you implement this bash script, please confirm you have already install Miniconda3 (best in version py37_4.9.2).\n \e[0m"
 
-read -t 10 -r -s -p $'Press enter to continue...' input
+read -s -n $'Press enter to continue...' input
 
 if [ "$input" = "" ]
 then
