e19e227423b2e07f12ff24f24436f9c72dfca61e
anitalu724 <b06901011@ntu.edu.tw>
2021-09-20 20:49:16 +0800

    20210920_quickInstall

--- mutscape/installation/quickInstall_1.sh
+++ mutscape/installation/quickInstall_1.sh
@@ -1,13 +1,13 @@
 
-echo -e "\e[1;33m \nWARNING: Before you implement this bash script, please confirm you have already install Miniconda3 (best in version py37_4.9.2).\n \e[0m"
+echo -e "\e[1;33m \nWARNING: Before you implement this bash script, please confirm you have already installed Miniconda3 (best in version py37_4.9.2) and created a new environment.\n \e[0m"
 echo -e "\e[1;33m Press enter to continue... \e[0m" 
 
 read -n 1 input
 if [ "$input" = "" ]
 then
-    echo -e "\e[1;35m Create a new Conda environment \n \e[0m"
-    conda create --name MutScape
-    activate MutScape
+    echo -e "\e[1;35m Install Ensembl's VEP \n \e[0m"
+    conda install -c bioconda -c conda-forge samtools=1.10 ucsc-liftover=377 bcftools=1.10.2
+    conda install -c bioconda -c conda-forge -c defaults ensembl-vep=102.0 
 else
     echo "Quit"
 fi
