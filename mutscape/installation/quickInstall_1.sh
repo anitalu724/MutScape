8b2e9c37e49ece31f88a23323ac73afe2bdbd5be
anitalu724 <b06901011@ntu.edu.tw>
2021-09-20 20:00:10 +0800

    20210920_quickInstall

--- mutscape/installation/quickInstall_1.sh
+++ mutscape/installation/quickInstall_1.sh
@@ -1,4 +1,12 @@
 echo -e "\e[1;33m \nThis is quickInstall_1.sh\n \e[0m"
 
-echo -e "Before you implement this bash script, please confirm you have already install Miniconda3 (best in version py37_4.9.2)."
+echo -e "\e[1;33m \nBefore you implement this bash script, please confirm you have already install Miniconda3 (best in version py37_4.9.2).\n \e[0m"
+
+if [[ $key = "" ]]; then 
+    echo 'You pressed enter!'
+else
+    echo "You pressed '$key'"
+fi
+
 echo -i "Press enter to continue..."
+
